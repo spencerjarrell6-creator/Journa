3c9c544ba23c2c16b4b1b63915bfb25013464958
Spencer Jarrell <spencerjarrell6@gmail.com>
2026-02-17 20:28:11 -0800

    Initial Commit

---
+++ Journa/JournaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaApp.swift
+//  Journa
+//
+//  Created by Spencer Jarrell on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct JournaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
